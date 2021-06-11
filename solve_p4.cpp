64566faf6cb3ee8b4cfb03394facd2fa275cac26
Mike Lam <lam2mo@jmu.edu>
2021-06-11 13:55:23 -0400

    Clean up P4 solution

--- solve_p4.cpp
+++ solve_p4.cpp
@@ -27,169 +27,172 @@ T absval(T v) {
 
 template <class T>
 T nthCoefficientProduct(vector<T> &x, vector<T> &y, int n){
-	/* This method computes the nth coefficient of x*y, runs in O(n)
-	 */
-	T sum = 0;
-	for(int i  = 0; i<=n; i++){
-		sum+=x[i]*y[n-i];
-	}
-	return sum;
+    /* This method computes the nth coefficient of x*y, runs in O(n)
+     */
+    T sum = 0;
+    for(int i  = 0; i<=n; i++){
+        sum+=x[i]*y[n-i];
+    }
+    return sum;
 }
 
 template <class T>
 void updateNthCoefficient(vector<T> &x, vector<T> &xx, vector<T> &xxx, int n){
-	/* This method asumes we have the n-th coefficient for x, it then updates the rest
-	 *
-	 * This method runs in O(N)
-	 */
-	 xx[n] = nthCoefficientProduct(x,x,n);
-	 xxx[n] = nthCoefficientProduct(xx,x,n);
+    /* This method asumes we have the n-th coefficient for x, it then updates the rest
+     *
+     * This method runs in O(N)
+     */
+    xx[n] = nthCoefficientProduct(x,x,n);
+    xxx[n] = nthCoefficientProduct(xx,x,n);
  }
 
 template <class T>
 vector<T> coefficients(T x0, int nn){
-	/* Inputs
-	 * n : order of the polynomial approximation
-	 * x0: initial conditions
-	 * Output:
-	 * coefficients of the nth polynomial approximation in increasing order
-	 *
-	 * Runs in O(n^2), memory O(n)
-	 */
-	 vector<T> x(nn,0);
-	 vector<T> xx(nn,0);
-	 vector<T> xxx(nn,0);
-	 vector<T> xprime(nn,0);
-	 x[0] = x0; //The initial condition is x0
-	 updateNthCoefficient<T>(x,xx,xxx,0);
+    /* Inputs
+     * n : order of the polynomial approximation
+     * x0: initial conditions
+     * Output:
+     * coefficients of the nth polynomial approximation in increasing order
+     *
+     * Runs in O(n^2), memory O(n)
+     */
+    vector<T> x(nn,0);
+    vector<T> xx(nn,0);
+    vector<T> xxx(nn,0);
+    vector<T> xprime(nn,0);
+    x[0] = x0; //The initial condition is x0
+    updateNthCoefficient<T>(x,xx,xxx,0);
 
-	 for(int n = 0; n<nn-1; n++){
-		 xprime[n] = xx[n]-xxx[n];
-		 x[n+1] = xprime[n]/(n+1);
-		 updateNthCoefficient<T>(x,xx,xxx,n+1);
-	 }
-	 return x;
+    for(int n = 0; n<nn-1; n++){
+        xprime[n] = xx[n]-xxx[n];
+        x[n+1] = xprime[n]/(n+1);
+        updateNthCoefficient<T>(x,xx,xxx,n+1);
+    }
+    return x;
 }
 
 template<class T>
 T eval(vector<T> coeff, T x){
-	/* Evaluates a Polynomial given its coefficients
-	 * Inputs:
-	 * coeff: coefficients of the polynomial to be evaluated in increasing order
-	 * x    : number to be evaluated
-	 * Output:
-	 * p(x)
-	 */
+    /* Evaluates a Polynomial given its coefficients
+     * Inputs:
+     * coeff: coefficients of the polynomial to be evaluated in increasing order
+     * x    : number to be evaluated
+     * Output:
+     * p(x)
+     */
 
-	//Horners Algorithm
-	T val = 0;
-	for(int i = coeff.size()-1; i>=0; i--){
-		val = val*x+coeff[i];
-	}
-	return val;
+    //Horners Algorithm
+    T val = 0;
+    for(int i = coeff.size()-1; i>=0; i--){
+        val = val*x+coeff[i];
+    }
+    return val;
 }
 
 template<class T>
 vector<T> derivative(vector<T> f){
-	vector<T> deriv(f.size()-1,0);
-	for(int i = 0; i< deriv.size(); i++){
-		deriv[i] = f[i+1]*(i+1);
-	}
-	return deriv;
+    vector<T> deriv(f.size()-1,0);
+    for(int i = 0; i< deriv.size(); i++){
+        deriv[i] = f[i+1]*(i+1);
+    }
+    return deriv;
 }
 
 template<class T>
 pair<T, T> computeNext(T x0, T step,bool forward,int n){
-	/*Inputs:
-	 * x0  : parameters of the ODE
-	 * step: Size of the step
-	 * forward: if you want to go forward or backwards
-	 * Outputs:
-	 * x(t) where t = x0+step and x solves the equation x' = x^2-x^3
-	 */
-	 vector<T> coeff = coefficients(x0,n);
-	 vector<T> deriv = derivative(coeff);
-	 if(forward){
-		return make_pair(eval<T>(coeff,step), eval<T>(deriv,step));
-	 }
-	 else{
-		return make_pair(eval<T>(coeff,-step), eval<T>(deriv,-step));
-	}
+    /*Inputs:
+     * x0  : parameters of the ODE
+     * step: Size of the step
+     * forward: if you want to go forward or backwards
+     * Outputs:
+     * x(t) where t = x0+step and x solves the equation x' = x^2-x^3
+     */
+    vector<T> coeff = coefficients(x0,n);
+    vector<T> deriv = derivative(coeff);
+    if(forward){
+       return make_pair(eval<T>(coeff,step), eval<T>(deriv,step));
+    }
+    else{
+        return make_pair(eval<T>(coeff,-step), eval<T>(deriv,-step));
+    }
 }
 
 template<class T>
 pair<vector<T>, vector<T> > generateSolutionStepper(T x0,T step, bool forward, T end, int n){
-	/*Inputs:
-	 * x0  : parameters of the ODE
-	 * step: Size of the step
-	 * end : End point to solve the solution (solves in [0,end])
-	 * n   : Degree of the approximation
-	 * forward: if you want to go forward or backwards
-	 * Output:
-	 * vector of solutions to the equation x' = x^2-x^3
-	 */
-	 step = absval(step);
-	 vector<T> sol;
-	 sol.push_back(x0);
-	 vector<T> deriv;
-     T xx = x0 * x0;
-	 deriv.push_back(xx-x0*xx);
-	 T newx0 = x0;
-	 for(int k = 1; step*k<=end; k++){
-		pair<T, T> cond = computeNext<T>(newx0,step,forward,n);
-		sol.push_back(cond.first);
-		deriv.push_back(cond.second);
-		newx0 = cond.first;
-	 }
-	 return make_pair(sol,deriv);
+    /*Inputs:
+     * x0  : parameters of the ODE
+     * step: Size of the step
+     * end : End point to solve the solution (solves in [0,end])
+     * n   : Degree of the approximation
+     * forward: if you want to go forward or backwards
+     * Output:
+     * vector of solutions to the equation x' = x^2-x^3
+     */
+    step = absval(step);
+    vector<T> sol;
+    sol.push_back(x0);
+    vector<T> deriv;
+    T xx = x0 * x0;
+    deriv.push_back(xx-x0*xx);
+    T newx0 = x0;
+    for(int k = 1; step*k<=end; k++){
+       pair<T, T> cond = computeNext<T>(newx0,step,forward,n);
+       sol.push_back(cond.first);
+       deriv.push_back(cond.second);
+       newx0 = cond.first;
+    }
+    return make_pair(sol,deriv);
 }
 
 template<class T>
 void solve_p4(T x0, T end, T step, int n, string fn)
 {
+    // generate solutions
     //cout << "size = " << sizeof(x0) << endl;
-	vector<T> coeff = coefficients(x0,n);
-	bool forward = 0;
-	vector<T> solutionStepperB = generateSolutionStepper<T>(x0,step,forward,end, n).first;
-	vector<T> derivSolutionB = generateSolutionStepper<T>(x0,step,forward,end, n).second;
-	forward = 1;
-	vector<T> solutionStepperF = generateSolutionStepper<T>(x0,step,forward,end, n).first;
-	vector<T> derivSolutionF = generateSolutionStepper<T>(x0,step,forward,end, n).second;
+    vector<T> coeff = coefficients(x0,n);
+    auto stepperB = generateSolutionStepper<T>(x0, step, 0, end, n);
+    vector<T> solutionStepperB = stepperB.first;
+    vector<T> derivSolutionB = stepperB.second;
+    auto stepperF = generateSolutionStepper<T>(x0, step, 1, end, n);
+    vector<T> solutionStepperF = stepperF.first;
+    vector<T> derivSolutionF = stepperF.second;
+
+    // print backward solution
     out.open(fn);
-	for(int i = solutionStepperB.size()-1; i>=0; i--){
-		int dig = 1000;
-		out.precision(5);
-		out<<fixed<<setw(15)<<-i*step<<" ";
-		T stepper = solutionStepperB[i];
-		T deriv = derivSolutionB[i];
+    for(int i = solutionStepperB.size()-1; i>=0; i--) {
+        int dig = 1000;
+        out.precision(5);
+        out << fixed << setw(15) << -i*step << " ";
+        T stepper = solutionStepperB[i];
+        T deriv = derivSolutionB[i];
+        if (absval(stepper) > dig || absval(deriv) > dig) {
+            out << scientific << setw(15) << stepper << " " << setw(15) << deriv << "\n";
+        }
+        else{
+            out << fixed << setw(15) << stepper << " " << setw(15) << deriv << "\n";
+        }
+    }
 
-		if( absval(stepper)>dig || absval(deriv)>dig ){
-			out<<scientific<<setw(15)<<stepper<<" "<<setw(15)<<deriv<<"\n";
-		}
-		else{
-			out<<fixed<<setw(15)<<stepper<<" "<<setw(15)<<deriv<<"\n";
-		}
-	}
-	for(int i = 1; i<solutionStepperF.size(); i++){
-		int dig = 1000;
-		out.precision(5);
-		out<<fixed<<setw(15)<<i*step<<" ";
-		T stepper = solutionStepperF[i];
-		T deriv = derivSolutionF[i];
-
-		if( absval(stepper)>dig || absval(deriv)>dig ){
-			out<<scientific<<setw(15)<<stepper<<" "<<setw(15)<<deriv<<"\n";
-		}
-		else{
-			out<<fixed<<setw(15)<<stepper<<" "<<setw(15)<<deriv<<"\n";
-		}
-	}
+    // print forward solution
+    for(int i = 1; i<solutionStepperF.size(); i++) {
+        int dig = 1000;
+        out.precision(5);
+        out << fixed << setw(15) << i*step << " ";
+        T stepper = solutionStepperF[i];
+        T deriv = derivSolutionF[i];
+        if (absval(stepper) > dig || absval(deriv) > dig) {
+            out << scientific << setw(15) << stepper << " " << setw(15) << deriv << "\n";
+        }
+        else{
+            out << fixed << setw(15) << stepper << " " << setw(15) << deriv << "\n";
+        }
+    }
     out.close();
 }
 
 int main(int argc, const char* argv[])
 {
-	// check parameters
+    // check parameters
     if (argc != 5) {
         cout << "Usage: " << argv[0] << " <x0> <tn> <dt> <rtype>" << endl;
         return EXIT_FAILURE;
